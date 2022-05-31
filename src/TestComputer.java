9a723d2f3bea44e51314cafa6e70c63b64f45dda
melchongx <103237225+melchongx@users.noreply.github.com>
2022-05-31 08:52:51 +0800

    added toHomePage method

--- src/TestComputer.java
+++ src/TestComputer.java
@@ -1,4 +1,24 @@
-public class TestComputer {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TestComputer extends JFrame implements ActionListener {
+    
+    public void toHomePage() {
+    removeAll();
+    //add(panel1); panel contains all components for home page
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            
+        } catch (Exception ex) {
+            //TODO: handle exception
+        }
+    }
     public static void main(String[] args) {
     }
+
+    
 }
