d89f60182fe8d909516cb55b25a7938f37a73a02
Marco <105768856+marcohernandez18@users.noreply.github.com>
2022-06-02 11:26:49 +0800

    Update TestComputer.java

--- src/TestComputer.java
+++ src/TestComputer.java
@@ -4,9 +4,43 @@ import javax.swing.*;
 
 public class TestComputer extends JFrame implements ActionListener {
     
+    JPanel Homepage = new JPanel();
+    //homepage.setBackground?
+    
+    JPanel SearchValid = new JPanel();
+    JPanel SearchInvalid = new JPanel();
+    JPanel CreateRecord = new JPanel();
+    JPanel ViewRecord = new JPanel();
+    JPanel EditRecord = new JPanel();
+    
     public void toHomePage() {
     removeAll();
     //add(panel1); panel contains all components for home page
+    }
+    
+    public void toSearchValid() {
+    removeAll();
+    //add(panel1); panel contains all components for search valid
+    }
+    
+    public void toSearchInvalid() {
+    removeAll();
+    //add(panel1); panel contains all components for search invalid
+    }
+    
+    public void toCreateRecord() {
+    removeAll();
+    //add(panel1); panel contains all components for create record
+    }
+    
+    public void toViewRecord() {
+    removeAll();
+    //add(panel1); panel contains all components for view record
+    }
+    
+    public void toEditRecord() {
+    removeAll();
+    //add(panel1); panel contains all components for edit record
     }
 
     @Override
